ed63ec68b024b150c0d324c42bf937bbe9f4752b
masterthodyu <masterthodyu@gmail.com>
2024-05-15 14:40:41 -0400

    comment out MORE

--- cogs/twitch_notifier/__init__.py
+++ cogs/twitch_notifier/__init__.py
@@ -1,6 +1,6 @@
 # This init is required for each cog.
 # Import your main class from the cog's folder.
-from .twitch_notifier import TwitchNotifier
+# from .twitch_notifier import TwitchNotifier
 
 
 # def setup(bot):
